b7936a58c0ee6678faa4acae22adb4f4d4751f1e
novikoviam <novikoviam@gmail.com>
2022-09-19 01:02:19 +0300

    Init commit from console

---
+++ src/test/java/git_hw_guru/qa/ExampleTest.java
@@ -0,0 +1,12 @@
+package git_hw_guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    void exampleTest(){
+        Assertions.assertTrue(2>1);
+    }
+}
